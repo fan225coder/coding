809219dc8251b7d52c3f61f055e349e73e9250de
fan225coder <fan225coder@gmail.com>
2018-07-27 00:47:14 -0600

    leet 159

--- leetcode/leet159.cpp
+++ leetcode/leet159.cpp
@@ -4,9 +4,9 @@ class Solution {
             const int n=s.length();
             const int k=2;
             unordered_map<char, int> sub;
-            int i=0,j=-1;
+            int j=-1;
             int len=0;
-            while (i<n) {
+            for (int i=0;i<n;i++) {
                 while (j+1<n) {
                     char ch=s[j+1];
                     if (sub.size()>=k && !sub.count(ch)) {
@@ -20,11 +20,9 @@ class Solution {
                 if (j==n-1) {
                     len=max(len, j-i+1);
                     break;
-                } else {
-                    if (sub[s[i]]==1) sub.erase(s[i]);
-                    else sub[s[i]]--;
-                    i++;
                 }
+                if (sub[s[i]]==1) sub.erase(s[i]);
+                else sub[s[i]]--;
             }
             return len;
         }
